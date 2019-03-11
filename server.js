0d8ecf529a20d737220a75a172e5c8ed6d7cd237
tkaing <t.kaing13@gmail.com>
2019-03-11 15:05:14 +0100

    Chat-bot

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
 
-app.get('/hello', function (req, res) {
-  res.send('Hello World')
+// Parsing
+app.use(express.json()); 
+
+// GET method route
+app.get('/hello', function (req, res) 
+{	
+	res.send('Hello World')
+})
+
+// POST method route
+app.post('/chat', function (req, res) 
+{		
+	if (req.body.msg === 'ville') {
+		res.send('Nous sommes à Paris');
+	}
+
+	if (req.body.msg === 'météo') {
+		res.send('Il fait beau');
+	}
 })
 
 var port = process.env.PORT || 3000;
+
 app.listen(port, function () {
   console.log('Example app listening on port ', port)
 })
