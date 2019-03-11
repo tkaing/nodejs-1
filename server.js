f0b8bf9021de1539dd94903e15a56b8956f93b0a
tkaing <t.kaing13@gmail.com>
2019-03-11 20:18:05 +0100

    Chat-bot avec mémoire

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 
 // Parsing
-app.use(express.json()); 
+app.use(express.json())
 
 // GET method route
-app.get('/hello', function (req, res) 
-{	
+app.get('/hello', function (req, res) {	
 	res.send('Hello World')
 })
 
 // POST method route
-app.post('/chat', function (req, res) 
-{		
-	if (req.body.msg === 'ville') {
-		res.send('Nous sommes à Paris');
+app.post('/chat', function (req, res) {		
+	// Message
+	var message = req.body.msg.split(' ')
+	var k = message[0]
+
+	// Read file
+	var content = fs.readFileSync('réponses.json')
+	var object = JSON.parse(content)
+
+	if (message.length === 1) {
+		(k in object) ? 
+		res.send(k + ': ' + object[k])
+		: res.send('Je ne connais pas ' + k + '...')
 	}
 
-	if (req.body.msg === 'météo') {
-		res.send('Il fait beau');
+	if (message.length === 3) {
+		// Set value to object
+		object[k] = message[2]
+
+		// Write in file
+		var data = JSON.stringify(object)
+		fs.writeFileSync('réponses.json', data)
+
+		res.send('Merci pour cette information !')
 	}
 })
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log('Example app listening on port ', port)
+	console.log('Example app listening on port ', port)
 })
